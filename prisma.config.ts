0ba018912b5b0c1d0208bbc7a511f68a4c56c11b
ussazimech <isragonzo07@gmail.com>
2026-07-02 14:34:17 -0400

    sixth commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,12 @@
+import "dotenv/config";
+import { defineConfig, env } from "@prisma/config";
+
 export default {
   datasource: {
     provider: "postgresql",
-    url: process.env.DATABASE_URL,
+    //url: env('DATABASE_URL'),
+    url:
+      process.env.DATABASE_URL ||
+      "postgresql://advmxdb_user:T3rysOUOKWSjVNJq7khKHqey60bnV2Ql@dpg-d93apbdaeets739u5670-a/advmxdb",
   },
 };
